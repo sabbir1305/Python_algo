b2ae10e7353cc1df6842a84cf625feb22314d773
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2022-12-08 00:15:46 +0600

    Introduction and Efficiency

---
+++ classines.py
@@ -0,0 +1,53 @@
+"""You can use this class to represent how classy someone
+or something is.
+"Classy" is interchangable with "fancy".
+If you add fancy-looking items, you will increase
+your "classiness".
+Create a function in "Classy" that takes a string as
+input and adds it to the "items" list.
+Another method should calculate the "classiness"
+value based on the items.
+The following items have classiness points associated
+with them:
+"tophat" = 2
+"bowtie" = 4
+"monocle" = 5
+Everything else has 0 points.
+Use the test cases below to guide you!"""
+
+class Classy(object):
+    def __init__(self):
+        self.items = []
+    def addItem(self, item):
+        self.items.append(item)
+        
+    def getClassiness(self):
+        classines = 0
+        for item in self.items:
+            if item == "tophat":
+                classines = classines + 2
+            elif item == "bowtie":
+                 classines = classines + 4
+            elif item =="monocle":
+                 classines = classines + 5
+        return  classines
+
+# Test cases
+me = Classy()
+
+# Should be 0
+print (me.getClassiness())
+
+me.addItem("tophat")
+# Should be 2
+print (me.getClassiness())
+
+me.addItem("bowtie")
+me.addItem("jacket")
+me.addItem("monocle")
+# Should be 11
+print (me.getClassiness())
+
+me.addItem("bowtie")
+# Should be 15
+print (me.getClassiness())
